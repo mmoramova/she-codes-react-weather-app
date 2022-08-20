446709bd231db80474551d22402feeb3d671c821
mmoramova <moramova@gmail.com>
2022-08-20 17:20:32 +0200

    added basic structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Weather from './Weather';
+import axios from 'axios';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1>
+          Hello world
+        </h1>
+        <Weather/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Weather() {
+    return (
+        <h2>
+            Hello from weather
+        </h2>
+    )
+}
