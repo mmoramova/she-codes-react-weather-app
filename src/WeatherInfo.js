2da83a369595255c82b4791226c7cce48a19624a
mmoramova <moramova@gmail.com>
2022-08-21 19:55:04 +0200

    added convertor

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FormattedDate from './FormattedDate';
 import WeatherIcon from "./WeatherIcon";
-import { AnimatedWeatherIcon } from 'animated-weather-icon';
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -22,12 +22,7 @@ export default function WeatherInfo(props) {
                     />
                     </div>
                     <div className="float-left">
-                        <span className="temperature">
-                        {Math.round(props.data.temperature)}
-                        </span>
-                        <span className="unit">
-                            C°
-                        </span>
+                    <WeatherTemperature celsius={Math.round(props.data.temperature)} />
                     </div>
                 </div>
             </div>
